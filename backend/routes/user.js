19c4be5d3760ea9378027fd8cbde9ae7b8fe9d81
PrabhashDVS <it20144012@my.sliit.lk>
2022-05-30 00:12:26 +0530

    bug-fixed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,16 +2,16 @@ const router = require("express").Router();
 
 
 const {
-  createLogin,
-  allLogins,
+  createUser,
+  allUsers,
   deleteLoginMember,
   deleteLoginMember2,
   login
 } = require('../controllers/userController');
 
 
-router.route("/add").post(createLogin);
-router.route("/login").get(allLogins);
+router.route("/add").post(createUser);
+router.route("/login").get(allUsers);
 router.route("/delete/:id").delete(deleteLoginMember);
 router.route("/deleteByUserID/:userID").delete(deleteLoginMember2);
 router.post("/login", login);
