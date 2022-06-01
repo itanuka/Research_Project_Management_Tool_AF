0ed3880e44796d81f97c71c540544337badfe51a
ZephyrClint <it20148768@my.sliit.lk>
2022-06-02 01:12:27 +0530

    Finished developing basic functionality of 'CoSupervisorList' component

--- frontend/src/components/user/staff/CoSupervisorList.js
+++ frontend/src/components/user/staff/CoSupervisorList.js
@@ -1,47 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from "framer-motion"
+import axios from 'axios'
 
 function CoSupervisorList() {
-    const co_supervisors = [
-        { id: 1, name: "Co-Supervisor 1", Interest: "Research Interest 1" },
-        { id: 2, name: "Co-Supervisor 2", Interest: "Research Interest 2" },
-        { id: 3, name: "Co-Supervisor 3", Interest: "Research Interest 3" },
-        { id: 4, name: "Co-Supervisor 4", Interest: "Research Interest 4" },
-        { id: 5, name: "Co-Supervisor 5", Interest: "Research Interest 5" }
-    ]
 
-  return (
-    <motion.div className='container'
-    initial={{  y:250}}
-    animate={{  y:0}}
-    exit={{ x: window.innerWidth, y: window.innerHeight }}
-    >
-        <h1 className='mt-3'> List of Co-Supervisors </h1>
-        <table className='table table-light table-striped table-hover mt-5'>
-            <thead>
-                <tr className='table-row'>
-                    <th scope='col'> Name </th>
-                    <th scope='col'> Interest </th>
-                    <th scope='col'> Request </th>
-                </tr>
-            </thead>
-            <tbody>
-                {co_supervisors.map(co_supervisor=>{
-                    return(<tr key={co_supervisor.id}>
-                        <td> { co_supervisor.name } </td>
-                        <td> { co_supervisor.Interest } </td>
-                        <td>
-                            <motion.button className='btn btn-primary px-4'
-                            initial={{backgroundColor:'white', transitionDuration:"3s"}}
-                            animate={{backgroundColor:'#015'}}
-                            > Request </motion.button>
-                        </td>
-                    </tr>)
-                })}
-            </tbody>
-        </table>
-    </motion.div>
-  )
+    const [staff, setStaff] = useState([])
+    const [coSupervisors, setCoSupervisors] = useState([])
+
+    async function getCoSupervisors() {
+        await axios.get("http://localhost:4000/api/v1/staff/")
+        .then((res)=> {
+            setStaff(res.data)
+        })
+
+        setCoSupervisors(
+            staff.filter((member)=>member.type === "co-supervisor")
+        )
+    }
+
+
+    useEffect(()=>{
+        getCoSupervisors()
+    }, [])
+
+    return (
+        <motion.div className='container'
+        initial={{  y:250}}
+        animate={{  y:0}}
+        exit={{ x: window.innerWidth, y: window.innerHeight }}
+        >
+            <h1 className='mt-3'> List of Co-Supervisors </h1>
+            <table className='table table-light table-striped table-hover mt-5'>
+                <thead>
+                    <tr className='table-row'>
+                        <th scope='col'> Name </th>
+                        <th scope='col'> ID Number </th>
+                        <th scope='col'> Research Interest </th>
+                        <th scope='col'> Request </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {coSupervisors.map(coSupervisor=>{
+                        return(<tr key={coSupervisor._id}>
+                            <td> { coSupervisor.name } </td>
+                            <td> { coSupervisor.idNumber } </td>
+                            <td> { coSupervisor.researchInterest } </td>
+                            <td>
+                                <motion.button className='btn btn-primary px-4'
+                                initial={{backgroundColor:'white', transitionDuration:"3s"}}
+                                animate={{backgroundColor:'#015'}}
+                                > Request </motion.button>
+                            </td>
+                        </tr>)
+                    })}
+                </tbody>
+            </table>
+        </motion.div>
+    )
 }
 
 export default CoSupervisorList
