153794db96d8402cafccf3e43e1d55248d359407
ZephyrClint <it20148768@my.sliit.lk>
2022-06-02 02:13:22 +0530

    added a search function to the SupervisorList component

--- frontend/src/components/user/staff/SupervisorList.js
+++ frontend/src/components/user/staff/SupervisorList.js
@@ -7,29 +7,47 @@ function SupervisorList() {
     const [staff, setStaff] = useState([])
     const [supervisors, setSupervisors] = useState([])
 
+    const [keyword, setKeyword] = useState("")
+
+    
     async function getSupervisors() {
         await axios.get("http://localhost:4000/api/v1/staff/")
         .then((res)=> {
             setStaff(res.data)
         })
-
+        
         setSupervisors(
             staff.filter((member)=>member.type==="supervisor")
         )
-    }
-    
-
+    }//end of getSupervisors function
+        
     useEffect(()=>{
         getSupervisors()
     }, [])
+        
+    // search function
+    const searchedSupervisors = supervisors.filter((supervisor) => {
+        return supervisor.name.toLowerCase().includes(keyword.toLowerCase())
+    })
+
 
   return (
     <motion.div
     initial={{  y:250}}
     animate={{  y:0}}
     exit={{ x: window.innerWidth, y: window.innerHeight }}
-    className='container'>
+    className='container' style={{justifyContent:"flex-end"}}>
         <h1 className='mt-3'> List of Supervisors </h1>
+
+        <div className='row mr-1'>
+            <div className='col-md-8'></div>
+            <input className="mt-5 col-md input-group-text" placeholder="Search Supervisor by Name..." style={{borderStyle:"initial"}}
+                onChange={(e) => {
+                setKeyword(e.target.value)
+                }}
+            />
+        </div>
+
         <table className='table table-light table-striped table-hover mt-5'>
             <thead>
                 <tr className='table-row'>
@@ -40,9 +58,9 @@ function SupervisorList() {
                 </tr>
             </thead>
             <tbody>
-                {supervisors.map(supervisor=>{
+                {searchedSupervisors.map(supervisor=>{
                     return(<tr key={supervisor.idNumber}>
-                        <td> { supervisor.type } </td>
+                        <td> { supervisor.name } </td>
                         <td> { supervisor.idNumber } </td>
                         <td style={{}}> { supervisor.researchInterest } </td>
                         <td>
