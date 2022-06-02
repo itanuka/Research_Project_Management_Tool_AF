a401d54d3ebe04c5ecabbdea05332ca45f080902
itanuka <it20149994@my.sliit.lk>
2022-06-02 22:03:34 +0530

    Student-Home and Group-Home added

--- frontend/src/components/user/student/GroupHome.js
+++ frontend/src/components/user/student/GroupHome.js
@@ -5,10 +5,9 @@ import { Link, useNavigate } from "react-router-dom";
 import UserSideBar from '../../layout/UserSideBar';
 import '../../style/studentHome.css'
 
-function StudentHome() {
+function GroupHome() {
 
   const [user, setUser] = useState({});
-  const [navigateLink, setNavigateLink] = useState("");
 
   const navigate = useNavigate();
 
@@ -17,9 +16,6 @@ function StudentHome() {
     try {
       const jwt = localStorage.getItem("token");
       setUser(jwtDecode(jwt));
-      let userObectID = jwtDecode(jwt).userObectID;
-      let navigateLink = "/students/view/" + userObectID;
-      setNavigateLink(navigateLink);
     } catch (error) {
 
     }
@@ -33,40 +29,42 @@ function StudentHome() {
         </div>
 
         <div className="col-10">
+          <div className="row">
+            <h1>Your group is {user.groupName}</h1>
+          </div>
+
           <div className="container">
             <div class="row row-cols-1 row-cols-md-2 mt-5">
-
-              {user.groupID ?
-                <>
-                  <div class="col mb-4">
-                    <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
-                      <div class="card-header text-center">Go to my group</div>
-                      <div class="card-body">
-                        <a href="/student/group" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </>
-                :
-                <>
-                  <div class="col mb-4">
-                    <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
-                      <div class="card-header text-center">Create Student Group</div>
-                      <div class="card-body">
-                        <a href="/student/createGroup" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
-                      </div>
-                    </div>
-                  </div>
-                </>}
-
 
 
 
               <div class="col mb-4">
                 <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
-                  <div class="card-header text-center">My Profile</div>
+                  <div class="card-header text-center">Topic Submission</div>
                   <div class="card-body">
-                    <a href={navigateLink} className="custom-size"><i class="fa-solid fa-file-circle-plus"></i></a>
+                    <a href="/student/group/topic" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div class="col mb-4">
+                <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
+                  <div class="card-header text-center">Download Templates</div>
+                  <div class="card-body">
+                    <a href="/student/createGroup" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div class="col mb-4">
+                <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
+                  <div class="card-header text-center">Upload Documents</div>
+                  <div class="card-body">
+                    <a href="/student/group/topic" className="custom-size"><i class="fa-solid fa-file-circle-plus"></i></a>
                   </div>
                 </div>
               </div>
@@ -99,4 +97,4 @@ function StudentHome() {
   );
 }
 
-export default StudentHome;
+export default GroupHome;
