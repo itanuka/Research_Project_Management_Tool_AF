5fe5fb5e3e7ab4a2eedc4fef857852f0679a1a19
itanuka <it20149994@my.sliit.lk>
2022-06-02 17:45:20 +0530

    If student has group,then link to that group wii be displayed. Otherwise create group link will be displayed

--- frontend/src/components/user/student/StudentHome.js
+++ frontend/src/components/user/student/StudentHome.js
@@ -32,14 +32,29 @@ function StudentHome() {
           <div className="container">
             <div class="row row-cols-1 row-cols-md-2 mt-5">
 
-              <div class="col mb-4">
-                <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
-                  <div class="card-header text-center">Create Student Group</div>
-                  <div class="card-body">
-                    <a href="/student/createGroup" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
+              {user.groupID ?
+                <>
+                  <div class="col mb-4">
+                    <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
+                      <div class="card-header text-center">Go to my group</div>
+                      <div class="card-body">
+                        <a href="/student/createGroup" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
+                </>
+                :
+                <>
+                  <div class="col mb-4">
+                    <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
+                      <div class="card-header text-center">Create Student Group</div>
+                      <div class="card-body">
+                        <a href="/student/createGroup" className="custom-size"><i class="fa-solid fa-user-group"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </>}
+
 
               <div class="col mb-4">
                 <div class="card text-white bg-dark mb-3" style={{ height: '240px', width: '400px' }}>
