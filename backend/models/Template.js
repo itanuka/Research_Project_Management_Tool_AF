c53161ca610fb8de6f042f1d47a876ad702fd6c3
Tharusha <it20143718@my.sliit.lk>
2022-06-02 22:22:02 +0530

    template model added

---
+++ backend/models/Template.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const templateSchema = mongoose.Schema(
+  {
+    template_title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    template_description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    file_path: {
+      type: String,
+      required: true
+    },
+    file_mimetype: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Template = mongoose.model('Template', templateSchema);
+
+module.exports = Template;
