e2b54b4b4ce65dcef9a5fb951665811eecf35bcb
Tharusha <it20143718@my.sliit.lk>
2022-06-04 01:20:41 +0530

    backend/app.js modified

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ const GroupRouter = require("./routes/group");
 const studentRequestsRouter = require("./routes/studentRequests");
 const topicRoute = require('./routes/topic');
 const templateRoute = require('./routes/template')
+const allocatedPanelMembers = require('./routes/allocatePanelMembers')
 
 app.use("/api/v1/students", StudentRouter);
 app.use("/api/v1/staff", StaffRouter);
@@ -29,6 +30,11 @@ app.use("/api/v1/topics", topicRoute);
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(topicRoute);
 app.use(templateRoute);
+app.use(allocatedPanelMembers);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
